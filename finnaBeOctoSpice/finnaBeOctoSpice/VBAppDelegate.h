3822e3802984f82a287ef22179b031762e60c637
Ariel Rodriguez <ariel@volonbolon.net>
2014-03-11 09:49:32 -0300

    Add Xcode project

---
+++ finnaBeOctoSpice/finnaBeOctoSpice/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  finnaBeOctoSpice
+//
+//  Created by Ariel Rodriguez on 3/11/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ finnaBeOctoSpice/finnaBeOctoSpice/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  finnaBeOctoSpice
+//
+//  Created by Ariel Rodriguez on 3/11/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
